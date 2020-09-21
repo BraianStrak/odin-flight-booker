261887f4131725d05431a1994d2a50115387797c
BraianStrak <braianstrak1999@gmail.com>
2020-09-21 22:04:02 +0100

    Succesfully passed chosen flight to booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,11 @@ class BookingsController < ApplicationController
     def new
         #pass it the parameters from submitting the last form
         @booking = Booking.new
+        params.each do |key, value|
+            puts "value below"
+            puts key.to_s + ' : ' + value
+        end
+        @flight = Flight.find(params[:flightChoice])
     end
 
     def index
